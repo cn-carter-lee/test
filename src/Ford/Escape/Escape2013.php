9f316c3675730a00c9fc93811130bc77ec2b1cc5
liqiang8080 <liqiang@dowedo.com.cn>
2017-12-27 14:26:35 +0800

    ...

---
+++ src/Ford/Escape/Escape2013.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Carter
+ * Date: 2017/12/27
+ * Time: 13:40
+ */
+
+namespace Ford\Escape;
+
+class Escape2013
+{
+    public static function info()
+    {
+        echo "This is Ford Escape2013!<br />";
+    }
+}
---
+++ src/Ford/Fiesta/Fiesta2013.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Carter
+ * Date: 2017/12/27
+ * Time: 13:43
+ */
+
+namespace Ford\Fiesta;
+
+class Fiesta2013
+{
+    public function info()
+    {
+        echo "This is Ford Fiesta2013!<br />";
+    }
+}
